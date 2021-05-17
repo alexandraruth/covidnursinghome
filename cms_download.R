1431ede58b7c21ed42a4b8831b87fdf576e70c70
alexandraruth <66081217+alexandraruth@users.noreply.github.com>
2021-05-16 20:20:40 -0400

    app shell complete & variables selected

--- cms_download.R
+++ cms_download.R
@@ -1,4 +1,7 @@
-## read in CMS nursing home COVID csv file
+
+
+
+##### Step 1: Read in CMS nursing home COVID csv file ##### 
 
 library(tidyverse)
 library(dplyr)
@@ -7,37 +10,54 @@ library(lubridate)
 cms = read_csv("nursinghome.csv")
 
 
-## convert date strings to "Date" variable in R
+##### Step 2: Wrangle data #####
+
+## date conversion ##
 
 cms = 
   cms %>% 
-    mutate(date_conversion=mdy(`Week Ending`))  # parses date string
-
-## subset just November 1 - December 31
+    mutate(date_conversion=mdy(`Week Ending`)) 
 
 
-## arrange truncated dataset by date and facility
+## subset by date & arrange ##
 
-cms_novdec = 
-  subset(cms, date_conversion > "2020-11-01" & date_conversion < "2020-12-31") %>%
+cms_testset = 
+  subset(cms, date_conversion > "2020-05-31" & date_conversion < "2020-12-31") %>%
   arrange(`Provider State`) %>%
-  arrange(`Federal Provider Number`) %>%
   arrange(date_conversion) %>%
-  mutate(ppe=0) %>%
-  mutate(staff=0) 
-  
+  arrange(`Federal Provider Number`) 
+ 
+## filter necessary variables ## 
 
+cms_testset = 
+  cms_testset %>% 
+  select(date_conversion, `Week Ending`, `Federal Provider Number`, `Provider Name`, `Provider Address`, `Provider City`, `Provider State`, `Provider Zip Code`, `Submitted Data`, `Passed Quality Assurance Check`, `Total Resident COVID-19 Deaths Per 1,000 Residents`, `Staff Total COVID-19 Deaths`, `Residents Total Confirmed COVID-19`, `Staff Total Confirmed COVID-19`, `Number of All Beds`, `Total Number of Occupied Beds`, `Shortage of Nursing Staff`, `Shortage of Clinical Staff`, `Shortage of Aides`, `Shortage of Other Staff`, `Any Current Supply of N95 Masks`, `Any Current Supply of Surgical Masks`, `Any Current Supply of Eye Protection`, `Any Current Supply of Gowns`, `Any Current Supply of Gloves`, `Any Current Supply of Hand Sanitizer`, `One-Week Supply of N95 Masks`, `One-Week Supply of Surgical Masks`, `One-Week Supply of Eye Protection`, `One-Week Supply of Gowns`, `One-Week Supply of Gloves`, `One-Week Supply of Hand Sanitizer`) 
 
 
 ## create PPE shortage variable
 
+if (cms$`Shortage of Aides` == 'Y')
 
-## create deaths variable
-
-
-## create facility size variable
+## create staff shortage variable (shortage_ppe)
 
 
-## plot deaths as function of 
- 
+
+## create facility size variable (facility_size)
+
+
   
+  
+
+#### Step 3: Test plots ####
+
+mymodel = glm(formula = shortage_staff ~ shortage_ppe + facility_size +  family = "binomial", data = cms_testset)
+summary(mymodel)
+
+
+
+#### Step 4: Save and push cleaned data to github ####
+
+
+
+
+
